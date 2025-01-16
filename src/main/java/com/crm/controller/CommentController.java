bd12bcead58e226e2006b81febec7ed004f5eb24
tushar-nig <72146200+tushar-nig@users.noreply.github.com>
2025-01-16 19:12:11 +0530

    Update CommentController.java

--- src/main/java/com/crm/controller/CommentController.java
+++ src/main/java/com/crm/controller/CommentController.java
@@ -22,6 +22,8 @@ public class CommentController {
     public String createComment(@RequestBody Comment comment, @RequestParam long postId)
     {
         System.out.println(1000);
+        System.out.println(1000); 
+        System.out.println(1000);
         System.out.println(1000);
         Post post=postRepository.findById(postId).get();
         comment.setPost(post);
